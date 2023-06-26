9d7f4a6f4c61738664bda0f9d7b7ee65a69f0f69
Tiago Monteiro Carvalho <107887124+MonteiroTiagoC@users.noreply.github.com>
2023-06-26 09:24:18 -0300

    Add files via upload

---
+++ Exercicios/ex003.py
@@ -0,0 +1,5 @@
+n1 = int(input('Insira um valor: '))
+n2 = int(input('Insira outro valor: '))
+
+S = n1 + n2
+print(f'A soma entre {n1} e {n2} é igual a {S}!')
---
+++ Exercicios/exExtra.py
@@ -0,0 +1,8 @@
+# Calculadora de equação do segundo grau
+
+a = int(input('Insira o valor de A: '))
+b = int(input('Insira o valor de B: '))
+c = int(input('Insira o valor de C: '))
+
+delta = (b**) - (4 * a * c)
+x1 = (-b + delta**(1/2))/(2 * a)
